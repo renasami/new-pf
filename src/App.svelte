082b3527f3f3b7ff6a236d662246dc8928162717
renasami <drrr0502@gmail.com>
2022-05-01 01:42:07 +0900

    app.svelte fixed

--- src/App.svelte
+++ src/App.svelte
@@ -29,29 +29,4 @@
     margin: 0 auto;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
 </style>
