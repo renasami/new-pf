f7808830bb57dd7dd7aaba9bed7213d78db6492e
renasami <drrr0502@gmail.com>
2022-05-01 12:01:21 +0900

    i18nが適用された

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,13 +1,19 @@
 <main>
     <Header>
     <h1>
-        about
+       {$_('about.page_title')}
     </h1>
     
     </Header>
 </main>
 <script lang="ts">
     import Header from "../components/layouts/Header.svelte"
-    import { _ } from 'svelte-i18n'
-
+    import { dictionary, locale, _ } from "svelte-i18n"
+    import * as en from "../i18n/en-US.json" 
+    import * as ja from "../i18n/ja.json" 
+    dictionary.set({
+        "en": {...en},
+        "ja": {...ja}
+    })
+    locale.set('en');
 </script>
