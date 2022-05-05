5ea09f9ec07c7fccc031e861d618782db5d4b7a5
ren <drrr0502@gmail.com>
2022-05-05 17:34:42 +0900

    add some

---
+++ src/components/templates/skills/[id].svelte
@@ -0,0 +1,19 @@
+<div>
+    <h2><a href={url} use:link>{ttl}</a></h2>
+    <img src={pic} alt={ttl}>
+    <p>
+        {description}
+    </p>
+</div>
+
+<script lang="ts">
+    import { link } from "svelte-routing"
+    export let ttl:string
+    export let pic:string
+    export let description:string
+    export let url:string
+</script>
+
+<style scoped>
+
+</style>
